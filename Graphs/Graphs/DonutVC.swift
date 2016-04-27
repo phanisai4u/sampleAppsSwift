016af7799937c3fd85f33b172964292c1c2dd744
Phanisai <phanisai@mobileways.in>
2016-04-27 18:53:45 +0530

    commit sample Graphs

--- Graphs/Graphs/DonutVC.swift
+++ Graphs/Graphs/DonutVC.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
-
+import Charts
 class DonutVC: UIViewController {
 
+    @IBOutlet weak var donutView: PieChartView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+        setChart(months, values: unitsSold)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +24,33 @@ class DonutVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setChart(dataPoints: [String], values: [Double]) {
+        
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let pieChartDataSet = PieChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
+        donutView.data = pieChartData
+        
+        var colors: [UIColor] = []
+        
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors
+        
+        }
 
     @IBAction func CloseButtonClick(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
--- Graphs/Graphs/LineGraphVC.swift
+++ Graphs/Graphs/LineGraphVC.swift
@@ -7,20 +7,55 @@
 //
 
 import UIKit
-
+import Charts
 class LineGraphVC: UIViewController {
 
+    @IBOutlet weak var lineChartView: LineChartView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+        
+        setChart(months, values: unitsSold)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    func setChart(dataPoints: [String], values: [Double]) {
+        
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+//        let pieChartDataSet = PieChartDataSet(yVals: dataEntries, label: "Units Sold")
+//        let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
+//        pieChartView.data = pieChartData
+//        
+//        var colors: [UIColor] = []
+//        
+//        for i in 0..<dataPoints.count {
+//            let red = Double(arc4random_uniform(256))
+//            let green = Double(arc4random_uniform(256))
+//            let blue = Double(arc4random_uniform(256))
+//            
+//            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+//            colors.append(color)
+//        }
+//        
+//        pieChartDataSet.colors = colors
+        
+        
+        let lineChartDataSet = LineChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
+        lineChartView.data = lineChartData
+        
+    }
 
     @IBAction func CloseButtonClick(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
